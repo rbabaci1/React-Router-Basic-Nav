ce7a2ea16961e71906effbc9542a737b1590dfe0
rabah babaci <4rabah@gmail.com>
2020-02-11 15:26:14 -0800

    create 3 routes for every nav item

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Home, About, Contact, Navigation } from "./components";
 import { Route } from "react-router-dom";
 
 const App = () => (
+  <Route path="/" component={Home}/>
+  <Route path="/about" component={About} />
+  <Route path="/contact" component={Contact} />
+
   <div>
     <Navigation />
   </div>
