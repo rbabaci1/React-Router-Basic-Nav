bd4962ab357560c1f1ac16a62d557b48866d55de
rabah babaci <4rabah@gmail.com>
2020-02-11 15:28:43 -0800

    import router dom into navigation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Home, About, Contact, Navigation } from "./components";
 import { Route } from "react-router-dom";
 
 const App = () => (
-  <Route path="/" component={Home}/>
-  <Route path="/about" component={About} />
-  <Route path="/contact" component={Contact} />
-
   <div>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+
     <Navigation />
   </div>
 );
